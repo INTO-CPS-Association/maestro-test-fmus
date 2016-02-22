e7a51c351c238304daf6eb7de721109bf7a60b0c
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-22 20:08:58 +0100

    first attempt at enabling wrapper build

--- xcompile.sh
+++ xcompile.sh
@@ -71,48 +71,84 @@ function compileLinux32
 		make -C $B		
 }
 
+
+function overtureToolWrapper
+{
+		wget http://overture.au.dk/into-cps/vdm-tool-wrapper/development/latest/vdm-tool-wrapper.zip -O vdm-tool-wrapper.zip
+
+
+		# resources
+		unzip -o -j vdm-tool-wrapper.zip vdm-tool-wrapper/resources/config.txt -d $1/resources/
+		unzip -o -j vdm-tool-wrapper.zip vdm-tool-wrapper/resources/fmi-interpreter-0.0.1-SNAPSHOT-jar-with-dependencies.jar -d $1/resources/
+
+		mkdir -p $1/binaries/
+		#tool wrapper binaries
+		unzip -o -j vdm-tool-wrapper.zip vdm-tool-wrapper/binaries/darwin64/libshmfmu.dylib -d $1/binaries/darwin64/
+		unzip -o -j vdm-tool-wrapper.zip vdm-tool-wrapper/binaries/win32/libshmfmu.dll -d $1/binaries/win32/
+		unzip -o -j vdm-tool-wrapper.zip vdm-tool-wrapper/binaries/win64/libshmfmu.dll -d $1/binaries/win64/
+		unzip -o -j vdm-tool-wrapper.zip vdm-tool-wrapper/binaries/linux64/libshmfmu.so -d $1/binaries/linux64/
+
+}
+
 function assemble
 {
 
-B=$1/build/
+		B=$1/build/
 
-mkdir -p $B/fmu/{binaries,resources,sources}
+		mkdir -p $B/fmu/{binaries,resources,sources}
 
-mkdir -p $B/fmu/binaries/{darwin64,win32,win64,linux32,linux64}
+		mkdir -p $B/fmu/binaries/{darwin64,win32,win64,linux32,linux64}
 
-echo Copying files...
-cp $1/modelDescription.xml $B/fmu/
+		echo Copying files...
+		cp $1/modelDescription.xml $B/fmu/
 
-cp $1/sources/*.* $B/fmu/sources/
+		cp $1/sources/*.* $B/fmu/sources/
 
-BIN=$B/fmu/binaries
-cp $B/darwin/*.dylib $BIN/darwin64/
-cp $B/linux64/*.so $BIN/linux64/
-cp $B/linux32/*.so $BIN/linux32/
-cp $B/win64/*.dll $BIN/win64/
-cp $B/win32/*.dll $BIN/win32/
+		BIN=$B/fmu/binaries
+		cp $B/darwin/*.dylib $BIN/darwin64/
+		cp $B/linux64/*.so $BIN/linux64/
+		cp $B/linux32/*.so $BIN/linux32/
+		cp $B/win64/*.dll $BIN/win64/
+		cp $B/win32/*.dll $BIN/win32/
 
-echo Zipping...
+		echo Zipping...
 
-curdir="$PWD"
+		curdir="$PWD"
 
-cd $B/fmu/
-zip -r ../$name.fmu .
+		cd $B/fmu/
+		zip -r ../$name.fmu .
 
-cd $curdir
+		cd $curdir
 }
 
-for D in `find . -maxdepth 1 -type d ! -path . ! -path *.git ! -path */includes ! -path */toolchains`
-do
-		echo "Folder is ${D}"
-name=`echo $D| sed 's|\./||g'`
-
+function xcompile
+{
+		D=$1
+		echo Compiling using CMake and make
 		compileDarwin $D
 		compileLinux64 $D
 		compileLinux32 $D
 		compileWin64 $D
 		compileWin32 $D
+}
 
+
+for D in `find . -maxdepth 1 -type d ! -path . ! -path *.git ! -path */includes ! -path */toolchains`
+do
+		echo "Folder is ${D}"
+		name=`echo $D| sed 's|\./||g'`
+		echo $D
+		if [ -e "$D/mode.txt" ] 
+		then
+				if grep -q OVERTURE_TOOL_WRAPPER "$D/mode.txt"; then
+						echo "Overture tool wrapper"
+						overtureToolWrapper $D
+				else
+						xcompile $D
+				fi
+		else
+				xcompile $D
+		fi
 		assemble $D $name
 done
 
