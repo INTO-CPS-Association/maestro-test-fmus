94e41cd83e187c8adbaaa6bdbac4db2d0a13e5e5
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-23 11:03:26 +0100

    update source code fmuid

--- cascading-watertank1-20sim/sources/fmiGUID.h
+++ cascading-watertank1-20sim/sources/fmiGUID.h
@@ -1,1 +1,1 @@
-#define FMI_GUID MODEL_GUID
+#define FMI_GUID "{4da879a6-0651-4db4-bb9e-72fa616b33a4}"
--- cascading-watertank2-20sim/sources/fmiGUID.h
+++ cascading-watertank2-20sim/sources/fmiGUID.h
@@ -1,1 +1,1 @@
-#define FMI_GUID MODEL_GUID
+#define FMI_GUID "{b214bd62-608a-466e-ab2e-28e49fe24b24}"
